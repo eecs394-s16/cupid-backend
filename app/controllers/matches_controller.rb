53b8f6e764c93a88bbd0e05134df27153c72ff0b
collin <barnwell.collin@gmail.com>
2016-04-06 23:23:11 -0500

    moved comment

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -16,10 +16,10 @@ class MatchesController < ApplicationController
     get_params
     puts @match_params
 
-    render json: {sup: 'hi'}
-    # Vote.match_vote(@match_params) if @match_params[:match_id]
-    # resp = User.find(@match_params[:user_id]).get_votable_match
+    Vote.match_vote(@match_params) if @match_params[:match_id]
 
+    render json: {sup: 'hi'}
+    # resp = User.find(@match_params[:user_id]).get_votable_match
 
     # render json: resp.to_json
   end
