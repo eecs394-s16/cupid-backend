c9675b51b4caf279da3f0c96bdeb75ecd9fcde55
collin <barnwell.collin@gmail.com>
2016-04-06 21:03:56 -0500

    removed cors route

--- config/application.rb
+++ config/application.rb
@@ -27,12 +27,6 @@ module CupidBackend
       allow do
         origins '*'
 
-        resource '/cors',
-          :headers => :any,
-          :methods => [:post],
-          :credentials => true,
-          :max_age => 0
-
         resource '*',
           :headers => :any,
           :methods => [:get, :post, :delete, :put, :patch, :options, :head],
