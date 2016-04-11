de82820d70322b25b76fb681863905b31e07917f
collin <barnwell.collin@gmail.com>
2016-04-11 10:42:20 -0500

    hello hi test

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,17 +4,8 @@ class VotesController < ApplicationController
     Vote.match_vote(@vote_params) if @vote_params[:match_id]
     resp = User.find(@vote_params[:user_id]).get_votable_match
 
-    m  = Match.where.not(user_1_id: 1).where.not(user_2_id: 2).all.shuffle.first
-    # render json: {
-    #   'match_id': m.id,
-    #   'user_id': 1,
-    #   'users': [
-    #      {'name': m.user1.full_name, 'profile_picture': m.user1.image_url},
-    #      {'name': m.user2.full_name, 'profile_picture': m.user2.image_url},
-    #   ]
-    # }
-
-    render json: resp.to_json
+    render json: {'hello': 'hi'}
+    # render json: resp.to_json
   end
 
   private
