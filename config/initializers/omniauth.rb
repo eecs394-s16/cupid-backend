0665a342ad31bd399efd81e28d97bfe26eba10df
NourAlharithi <nouralharithi2012@u.northwestern.edu>
2016-04-11 10:37:05 -0500

    initial commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+require 'omniauth-facebook'
+
+
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '226725207682997', 'f457e3ba5c343fb63cf28453b0d29351',
+           :scope => 'email', :display => 'popup'
+end
