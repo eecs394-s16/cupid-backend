f0b9e58bc77888644ccdc860cc64dfaaba6110c4
collin <barnwell.collin@gmail.com>
2016-04-11 11:35:55 -0500

    this will work because our spelling is spot on

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,18 +1,9 @@
 class VotesController < ApplicationController
   def create
     get_params
-    # Vote.match_vote(@vote_params) if @vote_params[:match_id]
-    # resp = User.find(@vote_params[:user_id]).get_votable_match
-
-    # http_envs = {}.tap do |envs|
-    #   request.headers.each do |key, value|
-    #     envs[key] = value if key.downcase.starts_with?('http')
-    #   end
-    # end
-
-    render json: {req: @vote_params}
-
-    # render json: resp.to_json
+    Vote.match_vote(@vote_params) if @vote_params[:match_id]
+    resp = User.find(@vote_params[:user_id]).get_votable_match
+    render json: resp.to_json
   end
 
   private
