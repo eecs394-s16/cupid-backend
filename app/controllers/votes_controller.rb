ac03b2071ec6d8c85b188dafd2f7252a9fd460f4
collin <barnwell.collin@gmail.com>
2016-04-11 11:20:31 -0500

    return request headers as response

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,18 @@
 class VotesController < ApplicationController
   def create
-    get_params
-    Vote.match_vote(@vote_params) if @vote_params[:match_id]
-    resp = User.find(@vote_params[:user_id]).get_votable_match
+    # get_params
+    # Vote.match_vote(@vote_params) if @vote_params[:match_id]
+    # resp = User.find(@vote_params[:user_id]).get_votable_match
 
-    render json: {'hello': 'hi'}
+    http_envs = {}.tap do |envs|
+      request.headers.each do |key, value|
+        envs[key] = value if key.downcase.starts_with?('http')
+      end
+    end
+
+
+    render json: {req: http_envs}
+
     # render json: resp.to_json
   end
 
