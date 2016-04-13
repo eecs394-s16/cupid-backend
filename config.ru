c65076dc476199f668eda1baf289e8015a8d4480
collin <barnwell.collin@gmail.com>
2016-04-13 10:46:35 -0500

    reenabled cors

--- config.ru
+++ config.ru
@@ -3,10 +3,10 @@
 require ::File.expand_path('../config/environment', __FILE__)
 run Rails.application
 
-# use Rack::Cors do
-#   allow do
-#     # origins 'localhost:3000', '127.0.0.1:3000', 'localhost:4567'
-#     origins '*'
-#     resource '', :headers => :any, :methods => :any
-#   end
-# end
+use Rack::Cors do
+  allow do
+    # origins 'localhost:3000', '127.0.0.1:3000', 'localhost:4567'
+    origins '*'
+    resource '', :headers => :any, :methods => :any
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -23,14 +23,14 @@ module CupidBackend
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    # config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
-    #   allow do
-    #     origins '*'
-    #     resource '*',
-    #       :headers => :any,
-    #       :methods => :any
-    #   end
-    # end
+    config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => :any
+      end
+    end
 
     config.active_record.raise_in_transactional_callbacks = true
   end
