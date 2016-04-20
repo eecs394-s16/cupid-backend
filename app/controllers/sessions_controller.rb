69503450c32e85aa7fdee2832d551102c93143e3
collin <barnwell.collin@gmail.com>
2016-04-20 13:39:27 -0500

    sessions controller works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
+    puts "!!!!!!!!!!!!!!!!!!!!!"
+    puts params
+    puts "!!!!!!!!!!!!!!!!!!!!!"
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       render json: { success: true, access_token: User.generate_access_token, user_id: user.id }
