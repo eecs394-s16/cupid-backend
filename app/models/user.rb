099f61b8cdf6b34051e3abebf4d770628988ae7e
jerryli27 <jerrylijiaming@gmail.com>
2016-04-25 12:00:17 -0500

    bug in friendship create
    The way to get uid was not correct

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
       friends.each do |profile|
         next unless friend = User.find_by_uid(profile['id'])
         #next if Friendship.where(user_id: user.id, friend_id: friend.id).blank?
-        Friendship.create({:user_id => auth['info']['uid'],:friend_id => profile['id']})
+        Friendship.create({:user_id => auth['uid'],:friend_id => profile['id']})
       end
 
     end
